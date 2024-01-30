ba88c4b3df540187337997b9f251659d29e05f8c
Juan Manuel De Cleene <jde-clee@c2r1s2.42madrid.com>
2024-01-30 18:11:31 +0100

    Makefiles & functions

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: jde-clee <jde-clee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 01:54:40 by jde-clee          #+#    #+#             */
-/*   Updated: 2024/01/30 01:26:03 by jde-clee         ###   ########.fr       */
+/*   Updated: 2024/01/30 17:54:26 by jde-clee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_stack	*ft_swap(t_stack *a)
 {
 	int aux;
 
+	if (ft_lstsize(a) < 2)
+		return (NULL);
 	aux = a->content;
 	a->content = a->next->content;
 	a = a->next;
@@ -25,15 +27,19 @@ t_stack	*ft_swap(t_stack *a)
 
 void	*ft_push(t_stack *a, t_stack *b)
 {
-	if (ft_lstsize(b) > 0)
+	if (ft_lstsize(a) > 0)
+	{
 		ft_lstadd_front(b, a);
-	ft_delone(a);
+		ft_delone(a);
+	}
 }
 
 void	*ft_rotate(t_stack *a)
 {
 	t_stack *aux;
 
+	if (ft_lstsize(a) < 2)
+		return ;
 	aux = ft_lstlast(a);
 	ft_delone(a);
 	ft_lstadd_back(a, aux);
@@ -43,7 +49,9 @@ void	*ft_reverse_rotate(t_stack *a)
 {
 	t_stack *aux;
 
-	aux = &a[0];
-	ft_lstclear(&a[0]);
+	aux = a;
+	if (ft_lstsize(a) < 2)
+		return ;
+	ft_lstdelone(a);
 	ft_lstadd_back(a, aux);
 }
